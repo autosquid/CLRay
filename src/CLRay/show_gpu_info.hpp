36da475b5dd7ec6c3f089f4fef297746a674e0a3
uuplusu <justin.seeley.cn@gmail.com>
2014-11-02 17:04:33 +0800

    framework to show gpu info

---
+++ src/CLRay/show_gpu_info.hpp
@@ -0,0 +1,5 @@
+#ifndef SHOW_GPU_INFO_HPP__
+#define SHOW_GPU_INFO_HPP__
+
+void show_gpu_config();
+#endif
--- src/app/raytracer.cpp
+++ src/app/raytracer.cpp
@@ -3,14 +3,8 @@
 #include <iostream>
 #include <cassert>
 
-int main() {
-    bool clpresent = 0 == clewInit();
-
-    if( !clpresent ) {
-        std::cout << "opencl library not found." << std::endl;
-        return -1;
-    }
-
+void show_gpu_config()
+{
     cl_int error = 0;   // Used to handle error codes
     cl_platform_id platform_id;
     cl_context context;
@@ -28,15 +22,15 @@ int main() {
     cl_uint num_devices;
     error = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device, &num_devices);
     if (error != CL_SUCCESS) {
-       std::cout << "Error getting device ids: " << error << std::endl;
-       exit(error);
+        std::cout << "Error getting device ids: " << error << std::endl;
+        exit(error);
     }
     std::cout << "num devices: " << num_devices << std::endl;
     cl_device_id *device_ids = new cl_device_id[num_devices];
     error = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, num_devices, device_ids, &num_devices);
     if (error != CL_SUCCESS) {
-       std::cout << "Error getting device ids: " << error << std::endl;
-       exit(error);
+        std::cout << "Error getting device ids: " << error << std::endl;
+        exit(error);
     }
     for( int i = 0; i < num_devices; i++ ) {
         device = device_ids[i];
@@ -53,13 +47,22 @@ int main() {
         printDeviceInfo( "frequency MHz", device, CL_DEVICE_MAX_CLOCK_FREQUENCY );
         std::cout << std::endl;
     }
-
-    return 0;
 }
 
 
+int main() {
+    bool clpresent = 0 == clewInit();
 
+    if( !clpresent ) {
+        std::cout << "opencl library not found." << std::endl;
+        return -1;
+    }
 
+    bool to_show_gpu_config = true;
 
+    if (to_show_gpu_config)
+        show_gpu_config();
 
+        return 0;
+}
 
